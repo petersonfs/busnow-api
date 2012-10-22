c5b5ccdd536111198a180756b98f9218388671f9
Peterson Ferreira <petersonferreiras@gmail.com>
2012-10-22 02:35:31 -0200

    adicionado a busca por tipo e linha de onibus

--- api.rb
+++ api.rb
@@ -9,9 +9,34 @@ class BusaoCwb < Sinatra::Base
 
   get "/api/schedules/:code.json" do |code|
     content_type :json
+
     items = []
 
     page = Nokogiri::HTML.parse(open(BASE_URL + "#{code}"), BASE_URL, "UTF-8")
+    name = page.css("h2.left.schedule")[0].content
+    page.css("div.bg-white.round-bl-60.width96.margin-medium-top.clearfix").each do |line|
+      station = line.css("h3.schedule")[0].content
+      type = line.css("p.grey.margin0")[0].content.match("([^-]+$)")[0].strip
+      schedules = line.css("li.bold").map { |s| s.content }
+      items << { :station => station, :type => type, :schedules => schedules }
+    end
+
+    Jbuilder.encode do |json|
+      json.name name
+      json.items items do |item|
+        json.station item[:station]
+        json.type item[:type]
+        json.schedules item[:schedules]
+      end
+    end
+  end
+
+  get "/api/schedules/:code/:type.json" do |code, type|
+    content_type :json
+
+    items = []
+
+    page = Nokogiri::HTML.parse(open(BASE_URL + "#{code}/#{type}"), BASE_URL, "UTF-8")
     name = page.css("h2.left.schedule")[0].content
     page.css("div.bg-white.round-bl-60.width96.margin-medium-top.clearfix").each do |line|
       station = line.css("h3.schedule")[0].content
