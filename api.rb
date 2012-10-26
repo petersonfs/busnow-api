373ad2af6e47cb8a061e13a629f47ecb983ab6ad
Peterson Ferreira <petersonferreiras@gmail.com>
2012-10-25 22:44:29 -0200

    Refactoring dos métodos que pegam os horarios por linha

--- api.rb
+++ api.rb
@@ -7,57 +7,46 @@ require "jbuilder"
 class BusNow < Sinatra::Base
   BASE_URL = "http://www.urbs.curitiba.pr.gov.br/horario-de-onibus/"
 
-  get "/api/schedules/:code.json" do |code|
+  get "/api/schedules"
     content_type :json
-
-    items = []
-
-    page = crawl_page "#{code}"
-    name = page.css("h2.left.schedule")[0].content
-    page.css("div.bg-white.round-bl-60.width96.margin-medium-top.clearfix").each do |line|
-      station = line.css("h3.schedule")[0].content
-      type = line.css("p.grey.margin0")[0].content.match("([^-]+$)")[0].strip
-      schedules = line.css("li.bold").map { |s| s.content }
-      items << { :station => station, :type => type, :schedules => schedules }
-    end
-
-    Jbuilder.encode do |json|
-      json.name name
-      json.items items do |item|
-        json.station item[:station]
-        json.type item[:type]
-        json.schedules item[:schedules]
-      end
-    end
   end
 
-  get "/api/schedules/:code/:type.json" do |code, type|
+  get "/api/schedules/:code" do |code|
     content_type :json
+    result = crawl_page "#{code}"
+    render result
+  end
 
-    items = []
-
-    crawl_page "#{code}/#{type}"
-
-    name = page.css("h2.left.schedule")[0].content
-    page.css("div.bg-white.round-bl-60.width96.margin-medium-top.clearfix").each do |line|
-      station = line.css("h3.schedule")[0].content
-      type = line.css("p.grey.margin0")[0].content.match("([^-]+$)")[0].strip
-      schedules = line.css("li.bold").map { |s| s.content }
-      items << { :station => station, :type => type, :schedules => schedules }
-    end
-
-    Jbuilder.encode do |json|
-      json.name name
-      json.items items do |item|
-        json.station item[:station]
-        json.type item[:type]
-        json.schedules item[:schedules]
-      end
-    end
+  get "/api/schedules/:code/:type" do |code, type|
+    content_type :json
+    result = crawl_page "#{code}/#{type}"
+    render result
   end
 
   private
   def crawl_page(url)
-    Nokogiri::HTML.parse(open(BASE_URL + url), BASE_URL, "UTF-8")
+    items = []
+    result = []
+    page = Nokogiri::HTML.parse(open(BASE_URL + url), nil, "UTF-8")
+    name = page.css("h2.left.schedule")[0].content
+    page.css("div.bg-white.round-bl-60.width96.margin-medium-top.clearfix").each do |line|
+      station = line.css("h3.schedule")[0].content
+      type = line.css("p.grey.margin0")[0].content.match("([^-]+$)")[0].strip
+      schedules = line.css("ul > li").map { |s| s.content }
+      items << { :station => station, :type => type, :schedules => schedules }
+    end
+    result << { :name => name, :items => items }
+  end
+
+  private
+  def render(result)
+    Jbuilder.encode do |json|
+      json.name result[0][:name]
+      json.items result[0][:items] do |item|
+        json.station item[:station]
+        json.type item[:type]
+        json.schedules item[:schedules]
+      end
+    end
   end
 end
